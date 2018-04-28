ca1f4982cff992372461493e98dc66f01e2852bf
gyz6666 <773803906@qq.com>
2018-04-28 17:48:49 +0800

    leetcode 10

---
+++ src/RegularExpressionMatching.java
@@ -0,0 +1,36 @@
+/**
+ * @author GYZ
+ * @DESCRIPTION
+ * @create 2018-04-28 17:47
+ **/
+public class RegularExpressionMatching {
+  public boolean isMatch(String s, String p) {
+    int n = s.length();
+    int m = p.length();
+    boolean[][] dp = new boolean[n + 1][m + 1];
+    dp[0][0] = true;
+    for (int j = 1; j <= m; j++)
+      if (p.charAt(j - 1) == '*')
+        dp[0][j] = dp[0][j - 2];
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= m; j++)
+      {
+        if (s.charAt(i - 1) == p.charAt(j - 1)
+            || p.charAt(j - 1) == '.')
+          dp[i][j] = dp[i - 1][j - 1];
+        if (p.charAt(j - 1) == '*')
+        {
+          if (p.charAt(j - 2) == s.charAt(i - 1)
+              || p.charAt(j - 2) == '.')
+            dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+          else
+            dp[i][j] = dp[i][j - 2];
+        }
+      }
+    }
+
+    return dp[n][m];
+
+  }
+}
