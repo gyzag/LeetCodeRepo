8d8a65f7fed2214d85eb984acab0331ba7172821
gyz6666 <773803906@qq.com>
2018-05-15 23:33:30 +0800

    leetcode 40

---
+++ src/CombinationSumII.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author GYZ
+ * @DESCRIPTION
+ * @create 2018-05-15 23:32
+ **/
+public class CombinationSumII {
+
+  public List<List<Integer>> combinationSum2(int[] nums, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, target, 0);
+    return list;
+
+  }
+
+  private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain,
+      int start) {
+    if (remain == 0) {
+      list.add(new ArrayList<>(tempList));
+    } else {
+      for (int i = start; i < nums.length; i++) {
+        if (i > start && nums[i] == nums[i - 1]) {
+          continue; // skip duplicates
+        }
+        if (nums[i] > remain) {
+          break;
+        }
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, remain - nums[i], i + 1);
+        tempList.remove(tempList.size() - 1);
+      }
+    }
+  }
+}
